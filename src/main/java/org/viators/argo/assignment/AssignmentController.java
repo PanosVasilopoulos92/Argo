256c464516b9f5398a60714d4c9cfdf61b1984be
panosV <panosVasilopoulos92@gmail.com>
2026-04-08 18:30:50 +0300

    feat(ARGO-20): Adds assignment fuctionality (assign seafarer to vessels) to project.

---
+++ src/main/java/org/viators/argo/assignment/AssignmentController.java
@@ -0,0 +1,31 @@
+package org.viators.argo.assignment;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.viators.argo.assignment.dto.request.CreateAssignmentRequest;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1/assignments")
+@RequiredArgsConstructor
+public class AssignmentController {
+
+    private final AssignmentService assignmentService;
+
+    @PreAuthorize("hasRole('FOM')")
+    @PostMapping
+    public ResponseEntity<String> create(@Valid @RequestBody CreateAssignmentRequest request) {
+        String createdAssignmentPublicId = assignmentService.create(request);
+        return ResponseEntity
+            .created(URI.create("/api/v1/assignments/" + createdAssignmentPublicId))
+            .body(createdAssignmentPublicId);
+    }
+
+}
