2ac086024bec3b0922023acf7ea13c7ad00e6af0
panosV <panosVasilopoulos92@gmail.com>
2026-04-14 16:27:45 +0300

    feat(ARGO-27): Adds person certificates functionality.

---
+++ src/main/java/org/viators/argo/certificate/person/PersonCertificateService.java
@@ -0,0 +1,46 @@
+package org.viators.argo.certificate.person;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.viators.argo.certificate.CertificateRepository;
+import org.viators.argo.certificate.CertificateT;
+import org.viators.argo.certificate.person.dto.request.CreatePersonCertificateRequest;
+import org.viators.argo.common.exceptions.BusinessValidationException;
+import org.viators.argo.common.exceptions.DuplicateResourceException;
+import org.viators.argo.common.exceptions.ResourceNotFoundException;
+import org.viators.argo.person.PersonRepository;
+import org.viators.argo.person.PersonT;
+import org.viators.argo.person.seafarer.SeafarerT;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PersonCertificateService {
+
+    private final CertificateRepository certificateRepository;
+    private final PersonCertificateRepository personCertificateRepository;
+    private final PersonRepository personRepository;
+
+    @Transactional
+    public String create(CreatePersonCertificateRequest request) {
+
+        PersonT person = personRepository.findByPublicId(request.personPublicId())
+            .orElseThrow(() -> new ResourceNotFoundException("Person", "publicId", request.personPublicId()));
+
+        if (certificateRepository.existsByCertificateNumber(request.certificateNumber())) {
+            throw new DuplicateResourceException("Certificate", "certificateNumber", request.certificateNumber());
+        }
+
+        if (request.expiryDate() != null && request.expiryDate().isBefore(request.issueDate())) {
+            throw new BusinessValidationException("Expiry date of certificate must be after issued date");
+        }
+
+        PersonCertificateT certificate = request.toEntity();
+        person.addCertificate(certificate);
+
+        personCertificateRepository.save(certificate);
+        return certificate.getPublicId();
+    }
+}
--- src/main/java/org/viators/argo/person/PersonRepository.java
+++ src/main/java/org/viators/argo/person/PersonRepository.java
@@ -3,8 +3,12 @@ package org.viators.argo.person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PersonRepository extends JpaRepository<PersonT, Long> {
 
     boolean existsByPassportNumber(String passportNumber);
+
+    Optional<PersonT> findByPublicId(String publicId);
 }
