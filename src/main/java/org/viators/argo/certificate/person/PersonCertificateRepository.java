dd21a7d7b9928a6dcd07392ac39cd1ea0405c6b0
panosV <panosVasilopoulos92@gmail.com>
2026-04-14 18:21:43 +0300

    feat(ARGO-29): Adds person's certifications retrieval functionality.

--- src/main/java/org/viators/argo/certificate/person/PersonCertificateRepository.java
+++ src/main/java/org/viators/argo/certificate/person/PersonCertificateRepository.java
@@ -1,5 +1,7 @@
 package org.viators.argo.certificate.person;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,8 @@ import java.util.Optional;
 public interface PersonCertificateRepository extends JpaRepository<PersonCertificateT, Long> {
 
     Optional<PersonCertificateT> findByPublicId(String publicId);
+
+    Page<PersonCertificateT> findByPerson_PublicId(String personPublicId, Pageable pageable);
 
     boolean existsByCertificateNumber(String certificateNumber);
 }
