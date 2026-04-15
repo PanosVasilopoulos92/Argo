890b396d6772ec4398e653ccf6ffa04d4549dd8f
panosV <panosVasilopoulos92@gmail.com>
2026-04-15 13:07:05 +0300

    feat(ARGO-32): Adds update certificate functionality.

--- src/main/java/org/viators/argo/certificate/CertificateRepository.java
+++ src/main/java/org/viators/argo/certificate/CertificateRepository.java
@@ -3,8 +3,12 @@ package org.viators.argo.certificate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CertificateRepository extends JpaRepository<CertificateT, Long> {
+
+    Optional<CertificateT> findByPublicId(String publicId);
 
     boolean existsByCertificateNumber(String certificateNumber);
 }
