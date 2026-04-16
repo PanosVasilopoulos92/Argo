417f98ab86c59bfac7b5b2ef92e15b5b70baacb6
panosV <panosVasilopoulos92@gmail.com>
2026-04-16 15:24:59 +0300

    feat(ARGO-39): Adds more details in SeafarerDetailsResponse record.

--- src/main/java/org/viators/argo/person/seafarer/dto/response/SeafarerDetailsResponse.java
+++ src/main/java/org/viators/argo/person/seafarer/dto/response/SeafarerDetailsResponse.java
@@ -1,13 +1,23 @@
 package org.viators.argo.person.seafarer.dto.response;
 
+import org.viators.argo.assignment.AssignmentStateEnum;
+import org.viators.argo.assignment.AssignmentT;
+import org.viators.argo.certificate.CertificateT;
+import org.viators.argo.certificate.enums.CertificateStatusIndicatorEnum;
+import org.viators.argo.certificate.person.PersonCertificateT;
+import org.viators.argo.certificate.vessel.VesselCertificateT;
 import org.viators.argo.common.enums.GenderEnum;
 import org.viators.argo.common.enums.ResourceStatusEnum;
 import org.viators.argo.person.seafarer.SeafarerT;
 import org.viators.argo.person.seafarer.enums.SeafarerRankEnum;
+import org.viators.argo.vessel.VesselT;
+import org.viators.argo.vessel.dto.response.VesselSummaryResponse;
 
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public record SeafarerDetailsResponse(
     String publicId,
@@ -32,7 +42,11 @@ public record SeafarerDetailsResponse(
     SeafarerRankEnum rank,
     String seamanBookNumber,
     LocalDate sbIssuedAt,
-    LocalDate sbExpiryDate
+    LocalDate sbExpiryDate,
+    Boolean hasActiveAssignment,
+    String vesselNameOfAssignment,
+    SeafarerRankEnum assignmentRank,
+    Map<String, Long> certificates
 ) {
 
     public static SeafarerDetailsResponse from(SeafarerT entity) {
@@ -59,7 +73,59 @@ public record SeafarerDetailsResponse(
             entity.getRank(),
             entity.getSeamanBookNumber(),
             entity.getSbIssuedAt(),
-            entity.getSbExpiryDate()
+            entity.getSbExpiryDate(),
+            hasActiveAssignment(entity.getAssignments()),
+            getAssignmentVessel(entity.getAssignments()),
+            getAssignmentRank(entity.getAssignments()),
+            defineCertificationValidation(entity.getCertificates())
         );
+    }
+
+    private static boolean hasActiveAssignment(Set<AssignmentT> assignments) {
+        return assignments.stream()
+            .anyMatch(a -> AssignmentStateEnum.ACTIVE.equals(a.getAssignmentState()));
+    }
+
+    public static String getAssignmentVessel(Set<AssignmentT> assignments) {
+        return assignments.stream()
+            .filter(a -> AssignmentStateEnum.ACTIVE.equals(a.getAssignmentState()))
+            .findFirst()
+            .map(AssignmentT::getVessel)
+            .map(VesselT::getVesselName)
+            .orElse(null);
+    }
+
+    public static SeafarerRankEnum getAssignmentRank(Set<AssignmentT> assignments) {
+        return assignments.stream()
+            .filter(a -> AssignmentStateEnum.ACTIVE.equals(a.getAssignmentState()))
+            .findFirst()
+            .map(AssignmentT::getAssignmentRank)
+            .orElse(null);
+    }
+
+    private static Map<String, Long> defineCertificationValidation(Set<PersonCertificateT> certificates) {
+
+        LocalDate today = LocalDate.now();
+        Map<String, Long> result  = certificates.stream()
+            .collect(Collectors.groupingBy(
+                c -> classifyCertificate(c, today),
+                Collectors.counting()
+            ));
+
+        Arrays.stream(CertificateStatusIndicatorEnum.values())
+            .forEach(s -> result.putIfAbsent(s.name(), 0L));
+
+        return result;
+    }
+
+    private static String classifyCertificate(PersonCertificateT c, LocalDate today) {
+
+        if (c.getExpiryDate().isBefore(today)) {
+            return CertificateStatusIndicatorEnum.EXPIRED.name();
+        } else if (c.getExpiryDate().isBefore(today.plusDays(90))) {
+            return CertificateStatusIndicatorEnum.EXPIRING_SOON.name();
+        } else {
+            return CertificateStatusIndicatorEnum.VALID.name();
+        }
     }
 }
