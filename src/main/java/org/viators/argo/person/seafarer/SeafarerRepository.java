06f8d7db8a5d464ff21d01cdd680333c1431425e
panosV <panosVasilopoulos92@gmail.com>
2026-04-16 15:24:59 +0300

    feat(ARGO-39): Adds more details in SeafarerDetailsResponse record.

--- src/main/java/org/viators/argo/person/seafarer/SeafarerRepository.java
+++ src/main/java/org/viators/argo/person/seafarer/SeafarerRepository.java
@@ -1,7 +1,10 @@
 package org.viators.argo.person.seafarer;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,6 +12,7 @@ import java.util.Optional;
 @Repository
 public interface SeafarerRepository extends JpaRepository<SeafarerT, Long>, JpaSpecificationExecutor<SeafarerT> {
 
+    @EntityGraph(attributePaths = {"assignments"})
     Optional<SeafarerT> findByPassportNumber(String passportNumber);
 
     Optional<SeafarerT> findByPublicId(String publicId);
@@ -17,4 +21,11 @@ public interface SeafarerRepository extends JpaRepository<SeafarerT, Long>, JpaS
 
     boolean existsBySeamanBookNumber(String seamanBookNumber);
 
+    @Query("""
+           select s from SeafarerT s
+           left join fetch s.assignments a
+           where s.publicId = :publicId
+           order by a.assignmentState asc
+           """)
+    Optional<SeafarerT> findByPublicIdWithAssignments(@Param("publicId") String publicId);
 }
