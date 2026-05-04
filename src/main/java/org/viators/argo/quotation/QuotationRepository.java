b4113e477d10863f922cf066aae7e6a0af216377
panosV <panosVasilopoulos92@gmail.com>
2026-05-04 15:28:51 +0300

    ARGO-69: Adds searching and filtering functionality for quotations.

--- src/main/java/org/viators/argo/quotation/QuotationRepository.java
+++ src/main/java/org/viators/argo/quotation/QuotationRepository.java
@@ -1,14 +1,39 @@
 package org.viators.argo.quotation;
 
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
-public interface QuotationRepository extends JpaRepository<QuotationT, Long> {
+public interface QuotationRepository extends JpaRepository<QuotationT, Long>, JpaSpecificationExecutor<QuotationT> {
 
     @EntityGraph(attributePaths = {"line", "supplier"})
     Optional<QuotationT> findByPublicId(String publicId);
+
+    @Override
+    @EntityGraph(attributePaths = {"line", "supplier"})
+    Page<QuotationT> findAll(@NonNull Specification<QuotationT> spec, @NonNull Pageable pageable);
+
+    @Query("select q from QuotationT q")
+    @EntityGraph(attributePaths = {"supplier", "line"})
+    Page<QuotationT> findAllForSummary(Pageable pageable);
+
+    @Query("""
+        select q from QuotationT q
+        where q.line.publicId = :linePublicId
+        order by q.quotationState
+        """)
+    @EntityGraph(attributePaths = {"line", "supplier"})
+    List<QuotationT> findAllQuotationsForLine(@Param("linePublicId") String linePublicId);
 }
--- src/main/java/org/viators/argo/requisition/RequisitionLineController.java
+++ src/main/java/org/viators/argo/requisition/RequisitionLineController.java
@@ -1,8 +1,14 @@
 package org.viators.argo.requisition;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.viators.argo.quotation.QuotationService;
+import org.viators.argo.quotation.dto.response.QuotationDetailsResponse;
+import org.viators.argo.quotation.dto.response.QuotationSummaryResponse;
 
 import java.util.List;
 
@@ -12,6 +18,12 @@ import java.util.List;
 public class RequisitionLineController {
 
     private final RequisitionLineRepository requisitionLineRepository;
+    private final QuotationService quotationService;
 
-    public List<>
+    @GetMapping("/{linePublicId/quotations}")
+    public ResponseEntity<List<QuotationSummaryResponse>> getQuotationsForLine(
+        @PathVariable String linePublicId
+    ) {
+        return ResponseEntity.ok(quotationService.getQuotationsForLine(linePublicId));
+    }
 }
