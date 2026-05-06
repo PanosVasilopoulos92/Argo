32f6ec70f470b5fc95ae2b0c4a44c6f2816869bb
panosV <panosVasilopoulos92@gmail.com>
2026-05-06 13:45:49 +0300

    ARGO-75: Adds functionality for sending POs to supplier

--- src/main/java/org/viators/argo/purchaseorder/PurchaseOrderRepository.java
+++ src/main/java/org/viators/argo/purchaseorder/PurchaseOrderRepository.java
@@ -1,5 +1,6 @@
 package org.viators.argo.purchaseorder;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,7 @@ import java.util.Optional;
 @Repository
 public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrderT, Long> {
 
+    @EntityGraph(attributePaths = {"supplier", "requisition", "poLines"})
     Optional<PurchaseOrderT> findByPublicId(String publicId);
 
 }
