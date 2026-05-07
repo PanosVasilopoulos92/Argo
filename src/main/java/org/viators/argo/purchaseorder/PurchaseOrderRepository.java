fa8677895f4a025b74ad831b7cd683759da59d3a
panosV <panosVasilopoulos92@gmail.com>
2026-05-07 11:36:57 +0300

    ARGO-80: Adds functionality for retrieving POs based on provided fitlters.

--- src/main/java/org/viators/argo/purchaseorder/PurchaseOrderRepository.java
+++ src/main/java/org/viators/argo/purchaseorder/PurchaseOrderRepository.java
@@ -1,15 +1,23 @@
 package org.viators.argo.purchaseorder;
 
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrderT, Long> {
+public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrderT, Long>, JpaSpecificationExecutor<PurchaseOrderT> {
 
     @EntityGraph(attributePaths = {"supplier", "requisition", "poLines"})
     Optional<PurchaseOrderT> findByPublicId(String publicId);
 
+    @Override
+    @EntityGraph(attributePaths = {"supplier", "requisition"})
+    Page<PurchaseOrderT> findAll(@NonNull Specification<PurchaseOrderT> spec, @NonNull Pageable pageable);
 }
