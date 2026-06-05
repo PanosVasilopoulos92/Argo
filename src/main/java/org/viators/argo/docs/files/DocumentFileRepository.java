ce7748ffe9fe462b1f2ee576ce319290dd192c67
panosV <panosVasilopoulos92@gmail.com>
2026-06-05 18:42:17 +0300

    ARGO-108: Adds functionality for displaying and downloading pdf doc files.

--- src/main/java/org/viators/argo/docs/files/DocumentFileRepository.java
+++ src/main/java/org/viators/argo/docs/files/DocumentFileRepository.java
@@ -3,6 +3,10 @@ package org.viators.argo.docs.files;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DocumentFileRepository extends JpaRepository<DocumentFileT, Long> {
+
+    Optional<DocumentFileT> findByStorageKey(String storageKey);
 }
